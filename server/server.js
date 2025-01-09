1466139faa81aaaa06eac405bc3b8456aa4bb9d8
aniket <aniketkoli17884@gmail.com>
2025-01-09 12:32:54 +0530

    Add coupon route

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import cors from 'cors';
 import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js';
 import cartRoutes from './routes/cart.route.js';
+import couponRoutes from './routes/coupon.route.js';
 
 dotenv.config();
 
@@ -28,6 +29,7 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/auth" , authRoutes)
 app.use("/api/product" , productRoutes)
 app.use("/api/cart" , cartRoutes)
+app.use("/api/coupons" , couponRoutes)
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`);
